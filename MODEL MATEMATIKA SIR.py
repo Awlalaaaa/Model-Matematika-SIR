408b5d2509821ae8568d5a3e7600b4497e76a308
Awlalaaaa <114721552+Awlalaaaa@users.noreply.github.com>
2022-12-21 13:45:39 +0700

    Add files via upload

---
+++ MODEL MATEMATIKA SIR.py
@@ -0,0 +1,38 @@
+Python 3.11.0 (v3.11.0:deaf509e8f, Oct 24 2022, 14:43:23) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import numpy as np
+... import matplotlib.pyplot as plt
+... 
+... to = 0
+... tn = 300
+... ndata = 1000
+... 
+... t = np.linspace(to,tn,ndata)
+... h = t[2]-t[1]
+... 
+... N = 1000 #rakyat
+... I0 = 1
+... R0 = 0
+... S0 = N - I0 - R0
+... 
+... I = np.zeros(ndata)
+... S = np.zeros(ndata)
+... R = np.zeros(ndata)
+... 
+... I[0] = I0
+... R[0] = R0
+... S[0] = S0
+... 
+... beta = 0.2
+... gamma = 0.1
+... 
+... for i in range (0,ndata-1):
+...     S[i+1] = S[i] - h*beta/N*S[i]*I[i]
+...     I[i+1] = I[i] + h*beta/N*S[i]*I[i] - h*gamma*I[i]
+...     R[i+1] = R[i] + h*gamma*I[i]
+... 
+... plt.plot(t,S,label = 'S')
+... plt.plot(t,I,label = 'I')
+... plt.plot(t,R,label = 'R')
+... 
+... plt.legend()
